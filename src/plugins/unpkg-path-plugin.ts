fe2c69576fe8552be420351edc1184a874f0b955
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 14:40:13 +0300

    Commented out the else case

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -13,13 +13,14 @@ export const unpkgPathPlugin = () =>
                 if (args.path === 'index.js')
                 {
                     return { path: args.path, namespace: 'a' };
-                } else if (args.path === 'tiny-test-pkg')
-                {
-                    return {
-                        path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-                        namespace: 'a',
-                    };
                 }
+                // else if (args.path === 'tiny-test-pkg')
+                // {
+                //     return {
+                //         path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+                //         namespace: 'a',
+                //     };
+                // }
             });
 
             build.onLoad({ filter: /.*/ }, async (args: any) =>
