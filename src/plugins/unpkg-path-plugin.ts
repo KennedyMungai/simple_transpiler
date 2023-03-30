29944a80a3b6b88e3506ace4f49cfd2fd3e85df2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 14:32:09 +0300

    Wrote a return statement for the build onLoad function

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -44,7 +44,10 @@ export const unpkgPathPlugin = () =>
 
                 const { data } = await axios.get(args.path)
 
-                console.log(data)
+                return {
+                    loader: 'jsx',
+                    contents: data
+                }
             });
         },
     };
