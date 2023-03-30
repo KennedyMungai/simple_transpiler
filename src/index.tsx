5fe7bfd281c63ee1e66ec13fc478a97ab4f072e6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 08:49:33 +0300

    Imported the useRef hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import * as esbuild from 'esbuild-wasm'
