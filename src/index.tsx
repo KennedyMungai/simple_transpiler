09bac2205a863a258b02130f31976555b96d4cb7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 08:30:48 +0300

    Made so that the button can only be pressed if the input state is not empty

--- src/index.tsx
+++ src/index.tsx
@@ -24,6 +24,7 @@ const App = () =>
             <div className="">
                 <button
                     onClick={onClick}
+                    disabled={!input}
                 >
                     Submit
                 </button>
