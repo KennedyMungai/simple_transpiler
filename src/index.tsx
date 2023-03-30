adb1d2cd39dbc26e527e559b9d698cf704a9f005
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 08:46:23 +0300

    Imported teh useEffect hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import * as esbuild from 'esbuild-wasm'
