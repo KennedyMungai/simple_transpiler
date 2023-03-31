a6f59f952ae5bbfdb9bbaadcc838f61bafa38fc9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 10:37:32 +0300

    A lot of changes were made

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const App = () =>
     {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: '/esbuild.wasm',
+            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
         });
     };
     useEffect(() =>
