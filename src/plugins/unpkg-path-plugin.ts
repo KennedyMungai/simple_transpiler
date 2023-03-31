7be1c9393b7ca44e927b8c7252535d3f987c33a7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 09:48:49 +0300

    Gave cached result a type of esbuild.OnLoadResult

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -58,7 +58,7 @@ export const unpkgPathPlugin = () =>
                     };
                 }
 
-                const cachedResult = await fileCache.getItem(args.path)
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
 
                 if (cachedResult)
                 {
