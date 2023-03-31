5caeb16b79c1352c198dba9fd8cfd9fb7c5a0db1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 10:19:15 +0300

    Some changes were made

--- src/index.tsx
+++ src/index.tsx
@@ -1,74 +1,62 @@
-import * as esbuild from 'esbuild-wasm'
-import { useEffect, useRef, useState } from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
-
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () =>
 {
-    const [input, setInput] = useState<string>('')
-    const [code, setCode] = useState<string>('')
-    const ref = useRef<any>()
+    const ref = useRef<any>();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
 
     const startService = async () =>
     {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: '/esbuild.wasm'
-        })
-    }
-
+            wasmURL: '/esbuild.wasm',
+        });
+    };
     useEffect(() =>
     {
-        startService()
-    }, [])
-
+        startService();
+    }, []);
 
     const onClick = async () =>
     {
         if (!ref.current)
         {
-            return
+            return;
         }
 
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin(input)],
+            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
             define: {
                 'process.env.NODE_ENV': '"production"',
-                global: 'window'
-            }
-        })
+                global: 'window',
+            },
+        });
 
-        setCode(result.outputFiles[0].text)
-    }
+        // console.log(result);
+
+        setCode(result.outputFiles[0].text);
+    };
 
     return (
-        <div className='transpiler-area'>
+        <div>
             <textarea
                 value={input}
-                onChange={e => setInput(e.target.value)}
-            >
-
-            </textarea>
-            <div className="">
-                <button
-                    onClick={onClick}
-                >
-                    Submit
-                </button>
+                onChange={(e) => setInput(e.target.value)}
+            ></textarea>
+            <div>
+                <button onClick={onClick}>Submit</button>
             </div>
-            {
-                code &&
-                <pre>
-                    {code}
-                </pre>
-            }
+            <pre>{code}</pre>
         </div>
-    )
-}
+    );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.querySelector('#root'));
